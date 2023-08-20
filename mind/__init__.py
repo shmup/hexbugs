26a0f99000fd713826db2b62389dceac8c78248c
Jared Miller <jared@smell.flowers>
2023-08-19 23:55:54 -0400

    Add DBHandler class and an addPlayer method

--- mind/__init__.py
+++ mind/__init__.py
@@ -3,12 +3,29 @@ import sqlite3
 import json
 from websockets.server import serve
 
+class DBHandler:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.init_db()
+
+    def init_db(self):
+        with self:
+            with open('schema.sql', 'r') as f:
+                self.cursor.executescript(f.read())
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        _ = exc_type, exc_val, exc_tb  # are you happy pyright
+        self.conn.commit()
+        self.conn.close()
+
 def add_player(name):
-    conn = sqlite3.connect('hexbugs.db')
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO players (name) VALUES (?)", (name,))
-    conn.commit()
-    conn.close()
+    with DBHandler('hexbugs.db') as cursor:
+        cursor.execute("INSERT INTO players (name) VALUES (?)", (name,))
 
 async def handle_message(websocket, path):
     print(path)
